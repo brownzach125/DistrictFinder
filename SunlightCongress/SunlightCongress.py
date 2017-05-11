53df9e3d402316d870aa57ad2b3989f77e62bb2d
Zach Brown <brownzach125@gmail.com>
2017-05-10 20:03:13 -0500

    Create wrapper to SunglightCongress Api
    Right now it just has a "get_district" function. That doesn't handle errors.
    Also I discovered that you don't need an api key to the api so that's nice.

---
+++ SunlightCongress/SunlightCongress.py
@@ -0,0 +1,21 @@
+import requests
+
+data = requests.get("https://congress.api.sunlightfoundation.com/legislators")
+
+sunlight_congress_domain = "https://congress.api.sunlightfoundation.com/legislators"
+
+
+def get_district(latitude, longitude):
+    payload = {
+        "latitude": latitude,
+        "longitude": longitude
+    }
+
+    request_str = sunlight_congress_domain + "/districts/locate"
+    data = requests.get(request_str, params=payload).json()
+
+    # TODO there is no error handling here :(
+    return data['results'][0]
+
+
+#get_district(29.748, -95.375)
