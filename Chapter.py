63bde58e55e61c82a42b04bff34837731523e04f
Zach Brown <brownzach125@gmail.com>
2017-05-17 22:54:02 -0500

    Second pass at chapter extraction from map
    Now uses the correct map. It doesn't extract state or district anymore, but gets the status now.

--- Chapter.py
+++ Chapter.py
@@ -14,10 +14,12 @@ class Chapter:
     @classmethod
     def from_kml_data(cls, info):
         name = info["name"]
-        state, district = tuple(info["district"].split('-'))
+        # TODO need to find this using lat and long
+        state, district = None, None
+        #state, district = tuple(info["district"].split('-'))
         lat, lng, _ = tuple(map(float,info["point"].strip().split(',')))
-        stat = None
-        # TODO determine status by pin color
+        # TODO this is just the style png which contains the color
+        stat = info['status']
 
         obj = cls(name, state, district, lat, lng, stat)
         return obj
@@ -26,5 +28,5 @@ class Chapter:
     def load_chapters_from_map():
         return [ Chapter.from_kml_data(x) for x in PinExtractor.get_chapter_info()]
 
-#chapters = Chapter.load_chapters_from_map()
+chapters = Chapter.load_chapters_from_map()
 
