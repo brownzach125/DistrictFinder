9dfefff7e039097ab8c5750e5e1559055a0dfe40
alexandrasummers <cysteinechapels@gmail.com>
2017-05-17 21:15:24 -0500

    Alex has preliminary working code! Hard coded petro metro files not uploaded.

--- Chapter.py
+++ Chapter.py
@@ -5,9 +5,9 @@ class Chapter:
     def __init__(self, name, state, district, lat, lng, stat):
         self.name = name
         self.state = state
-        self.district = district
-        self.lat = lat
-        self.lng = lng
+        self.district = int(district)
+        self.lat = float(lat)
+        self.lng = float(lng)
         self.stat = stat
 
     @classmethod
@@ -27,9 +27,3 @@ class Chapter:
 
 #chapters = Chapter.load_chapters_from_map()
 
-filename = "Petro_Metro_Chapters.csv"
-
-PM_Chapters = []
-
-# I want to be able to read a CSV....
-# but instead of creating a new dict out of each row, create an instance of Chapter instead
--- main.py
+++ main.py
@@ -3,13 +3,27 @@ from SunlightCongress import SunlightCongress
 import pprint
 import csv
 import sys
-import Chapter
+from Chapter import Chapter
 
+filename = "Petro_Metro_Chapters.csv"
+
+PM_Chapters = []
+
+with open(filename, 'rU') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        PM_Chapters.append(Chapter(*row))
+
+
+# print PM_Chapters
 
 def from_address_to_district(location):
     g = geocoder.google(location)
     coords = g.latlng
-    return SunlightCongress.get_district(*coords)
+    dict1 = {'Latitude': coords[0], 'Longitude': coords[1]}
+    dict2 = SunlightCongress.get_district(*coords)
+    dict1.update(dict2)
+    return dict1
 
 
 # filename = sys.argv[1]
@@ -22,18 +36,24 @@ with open(filename, 'rU') as csvfile:
     for row in reader:
         entries.append(row)
 
+
 for item in entries:
     item.update(from_address_to_district(item['Address']))
 
-distance = 2 ^ 5
-# for item in entries:
-#   for chapter in PM_chapters:
-#       if item['district']=chapter.district
-#
-#
-fieldnames = entries[0].keys()
+closest_chapter = 'Blank'
+for item in entries:
+    distance = 2 ^ 5
+    for chapter in PM_Chapters:
+        if item['district'] == chapter.district:
+            distance_new = ((chapter.lat - item['Latitude']) ** 2 + (chapter.lng - item['Longitude']) ** 2) ** .5
+            if distance_new < distance:
+                distance = distance_new
+                item['chapter'] = chapter.name
 
-with open(filename, 'wb') as f:
-    w = csv.DictWriter(f)
-    w.writeheader()
-    w.writerows(entries)
+print entries
+# fieldnames = entries[0].keys()
+
+# with open(filename, 'wb') as f:
+#    w = csv.DictWriter(f)
+#    w.writeheader()
+#    w.writerows(entries)
