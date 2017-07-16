9b1a72e44fec58fca13cf32c8fd3a070b51b4dd6
Zach Brown <brownzach125@gmail.com>
2017-07-16 14:55:44 -0500

    More Email Stuff

--- Email/Email.py
+++ Email/Email.py
@@ -11,34 +11,46 @@ Subject: SMTP e-mail test
 This is a test e-mail message.
 """
 
+def send_email(to_name, to_email, sender_name, sender_email, subject):
+   from_line = "From: {0} <{1}>".format(sender_name, sender_email)
+   to_line = "To: {0} <{1}>".format(to_name, to_email)
+   subject_line = "Subject: {0}".format(subject)
+
+   message = """From: {0} <{1}>
+To: {2} <{3}>
+Subject: {4}
+
+Dear {2}
+I hope Alex has remembered to change this string to whatever it's supposed to be.
+In fact It'd be even better if she changed this to a txt file she loaded in.
+
+With the best wishes
+{0}
+""".format(sender_name, sender_email, to_name, to_email, subject)
+
+   try:
+      server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+      server.ehlo()
+      server.starttls()
+
+      env_variable = "EMAIL_KEY_FOR_CHARITY"
+      key = os.environ.get(env_variable)
+      if not key:
+         print "Error: " + env_variable + " is not set in the environment read the code for help."
+         return False
+
+      server.login(sender_email, key)
+      server.sendmail(sender_email, [to_email], message)
+      server.close()
+      return True
+   except smtplib.SMTPException as err:
+      print "Error: unable to send email"
+      return False
+
+send_email(to_name="Zach Brown", to_email="brownzach125@gmail.com",
+           sender_email="brownzach125@gmail.com", sender_name="solevi", subject="FUCK YOU")
 
 
 
-#####
-## GET YOUR SECRET SPEICAL PASSWORD FROM HERE
-## security.google.com/settings/security/apppasswords
-#####
-
-############
-# Setting up environment variables
-# go to run->editconfigurations
-# click on the program whose variables you want to edit (the script you want to run this code from some maybe main or email.py
-# look for environment variables
-# click the ... to the right
-# add EMAIL_KEY_FOR_CHARITY=<your key whatever it is>
-###
 
 
-try:
-   server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-   server.ehlo()
-   server.starttls()
-
-   key = os.envirion('EMAIL_KEY_FOR_CHARITY')
-
-   server.login("the account you want to send emails from", key)
-   server.sendmail(sender, receivers, message)
-   server.close()
-   print "Successfully sent email"
-except smtplib.SMTPException as err:
-   print "Error: unable to send email"
